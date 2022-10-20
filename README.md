aff90c5a495891c5c15fa52cad1e2f1b9432f683
Alexandru Bordei <alexandru.bordei@gmail.com>
2022-10-20 15:06:06 +0300

    Added server2

--- README.md
+++ README.md
@@ -1,8 +1,25 @@
 # MetalSoft CI/CD example using github actions
 
-Example Terraform MetalSoft manifest that can be used as part of a CI/CD pipeline.
+Example Terraform MetalSoft manifest that can be used as part of a CI/CD pipeline. This setup is configured to automatically push changes onto a demo environment.
 
-## To test
+To test the automation:
+1. Clone the repository locally:
+```
+git clone git@github.com:alexandrubordei/metalsoft-cicd-test.git
+```
+2. Edit the infrastructure blueprint, for example add another cluster such as `server3` in `example.tf`. Don't forget to add a dependency in the `infrastructure_deployer` resource to point to the newly created server.
+3. Commit & push the change
+```
+git add .
+git commit -m "Added server3 as requested by management"
+git push origin main
+```
+4. Go to the infrastructure editor and see the changes. Note that the infrastructure is created under my user.
+> Note that to actually deploy change the 
+`prevent_deploy` flag on the `infrastructure_deployer` to `false`
+
+## To test locally
+
 Configure the following env variables. Note that the server type and volume types are also environment dependent.
 ```
 
@@ -21,4 +38,5 @@ terraform plan
 ```
 terraform apply
 ```
+
 
--- example.tf
+++ example.tf
@@ -32,11 +32,29 @@ data "metalcloud_server_type" "large"{
   server_type_name = "M.8.8.1.v2"
 }
 
-resource "metalcloud_instance_array" "cluster" {
+resource "metalcloud_instance_array" "server1" {
 
     infrastructure_id = data.metalcloud_infrastructure.infra.infrastructure_id
 
-    instance_array_label = "test-3"
+    instance_array_label = "test-1"
+
+    instance_server_type{
+      instance_index=0
+      server_type_id=data.metalcloud_server_type.large.server_type_id
+    }
+
+    volume_template_id = tonumber(data.metalcloud_volume_template.ubuntu20.id)
+
+    instance_array_firewall_managed = false
+
+}
+
+
+resource "metalcloud_instance_array" "server2" {
+
+    infrastructure_id = data.metalcloud_infrastructure.infra.infrastructure_id
+
+    instance_array_label = "test-2"
 
     instance_server_type{
       instance_index=0
@@ -71,6 +89,7 @@ resource "metalcloud_infrastructure_deployer" "infrastructure_deployer" {
   # IMPORTANT. This is important to ensure that deploys happen after everything else. If you need to add or remove resources dynamically
   # use either count or for_each in the resources or move everything that is dynamic into a module and make this depend on the module
   depends_on = [
-    metalcloud_instance_array.cluster,
+    metalcloud_instance_array.server1,
+    metalcloud_instance_array.server2,
   ]
 }
